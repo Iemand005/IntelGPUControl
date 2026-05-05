3efe026f90dd78593a4ce843fc80460867ff9b57
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-06 00:00:22 +0200

    App base

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 
 #include <wx/wx.h>
 
-int main() {
-  zeInit(ZE_INIT_FLAG_GPU_ONLY);
+// int main() {
+//   zeInit(ZE_INIT_FLAG_GPU_ONLY);
 
 
-}
+// }
+
+class IntelGPUControlApp : public wxApp {
+public:
+    virtual bool OnInit() override {
+        wxFrame *frame = new wxFrame(NULL, wxID_ANY, "Hello World", wxDefaultPosition, wxSize(450, 340));
+        
+        frame->Show(true);
+        
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(IntelGPUControlApp);
