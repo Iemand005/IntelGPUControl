98864b7c77bbed56b48e48c9a96b3c2ca3672775
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-07 11:13:39 +0200

    dang

--- main.cpp
+++ main.cpp
@@ -60,7 +60,7 @@ public:
     }
 
     zes_freq_range_t GetSupportedFrequencyRange() {
-        zes_freq_properties_t Properties;
+        zes_freq_properties_t Properties{};
         zesFrequencyGetProperties(hFrequency, &Properties);
         
         // if (!Properties.canControl)
@@ -85,7 +85,7 @@ public:
     //     }
 
     zes_freq_range_t GetFrequencyRange() {
-        zes_freq_range_t limits;
+        zes_freq_range_t limits{};
         if (zesFrequencyGetRange(hFrequency, &limits) == ZE_RESULT_SUCCESS)
             return limits;
         throw std::runtime_error("Failed to get frequency limits.");
@@ -132,7 +132,7 @@ public:
 
             panel->SetSizer(sizer);
 
-            auto updateSliders = [=]() {
+            auto updateSliders = [&]() {
                 auto range = GetFrequencyRange();
                 minSlider->SetValue(range.min);
                 maxSlider->SetValue(range.max);
